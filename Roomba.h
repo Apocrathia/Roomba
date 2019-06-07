8171232cf7d1a09efbb262a9f078ae3dbc0e68a0
CTXz <ctx.xda@gmail.com>
2019-06-07 16:21:10 +0200

    Roomba: Check if Arduino has a HW serial or SW serial
    Some Arduino models, such as the UNO, do not offer a hardware serial and simply share their RX(0) and TX(1) pins with their FTDI usb to serial chip. The default Serial class in these models is an instance of Serial_, not HardwareSerial. Some models, such as the Arduino Leonardo, do offer a seperate HW serial communication on pins RX(0) and TX(1) via the Serial1 object, which is an instance of HardwareSerial. In these models, the "Serial" object is explicitly reserved for the FTDI chip only! Finally, in some more powerful Arduino models, such as SAM cores, the default "Serial" object is an instance of HardwareSerial.
    
    Signed-off-by: CTXz <ctx.xda@gmail.com>

--- Roomba.h
+++ Roomba.h
@@ -386,7 +386,13 @@ public:
     /// \param[in] serial POinter to the HardwareSerial port to use to communicate with the Roomba.
     /// Defaults to &Serial
     /// \param[in] baud the baud rate to use on the serial port. Defaults to 57600, the default for the Roomba.
-    Roomba(HardwareSerial* serial = &Serial, Baud baud = Baud57600);
+    #ifdef HAVE_HWSERIAL0
+      Roomba(HardwareSerial* serial = &Serial, Baud baud = Baud57600);
+    #elif defined HAVE_HWSERIAL1
+      Roomba(HardwareSerial* serial = &Serial1, Baud baud = Baud57600);
+    #else
+      Roomba(Serial_* serial = &serial, Baud baud = Baud57600);
+    #endif
 
     /// Resets the Roomba.
     /// It will emit its startup message
